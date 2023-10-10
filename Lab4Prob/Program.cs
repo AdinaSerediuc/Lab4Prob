a7006c79a057764735be946abce4e9d3818c4995
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-10-10 22:46:54 +0300

    Calculare Volum

--- Lab4Prob/Program.cs
+++ Lab4Prob/Program.cs
@@ -7,8 +7,8 @@ namespace Lab4Prob
     class Program
     {
         static void Main(string[] args)
-            /*
-             * Ex 1
+        /*
+         * Ex 1
 Un dulap are trei caracteristici : lungime, latime, inaltime. Dulapul va avea o
 metoda care va calcula volumul acestuia.
 Folositi o clasa care va modela un dulap.
@@ -17,26 +17,44 @@ Folositi o clasa care va modela un dulap.
  Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
 Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si
 afisati rezultatul.
-            */
+        */
         {
-             Dulap dulap = new Dulap();
-            dulap.lungime = 200;
-            dulap.latime = 250;
-            dulap.inaltime = 230;
+            Dulap dulap = new Dulap();
+            dulap.lungime = 2.5;
+            dulap.latime = 1.0;
+            dulap.inaltime = 2.0;
 
-        //Console.WriteLine($"Volumul Dulapului este {dulap.volum} ");
+            dulap.volum = dulap.CalculeazaVolum();
+
+            PrintDulap(dulap);
+            PrintVolum(dulap);
 
         }
-        static void PrintDulap (Dulap dulap)
+        static void PrintDulap(Dulap dulap)
         {
             Console.WriteLine($"Dimensiunile unui dulap sunt {dulap.lungime} , {dulap.latime} , {dulap.inaltime} ");
         }
+
+        static void PrintVolum(Dulap dulap)
+        {
+            Console.WriteLine($"Volumul Dulapului este {dulap.volum} ");
+        }
+        class Dulap
+        {
+            public double lungime;
+            public double latime;
+            public double inaltime;
+            public double volum;
+
+            public double CalculeazaVolum()
+            {
+                volum = lungime * latime * inaltime;
+                return volum;
+            }
+        }
+
     }
-    class Dulap
-    {
-        public double lungime;
-        public double latime;
-        public double inaltime;
-    }
-        
 }
+
+        
+
